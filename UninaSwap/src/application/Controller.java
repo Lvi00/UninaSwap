3ce72ccc7f3288024009747d4b62f2008ca279be
casta <casta@Pc-Luigi.mshome.net>
2025-07-15 20:34:14 +0200

    Autenticazione migliorata

--- UninaSwap/src/application/Controller.java
+++ UninaSwap/src/application/Controller.java
@@ -1,5 +1,71 @@
 package application;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Controller {
+
+	public String checkData(String nome, String cognome, String matricola, String email, String username, String password) {
 	
+	    if ((nome.length() > 40 || nome.length() < 2) || isValidNameSurname(nome) == 1) {
+	        System.out.println("Nome non valido.");
+	        return "nome";
+	    }
+
+	    
+	    if ((cognome.length() > 40 || cognome.length() < 2) || isValidNameSurname(cognome) == 1) {
+	        System.out.println("Cognome non valido..");
+	        return "cognome";
+	    }
+	    
+	    
+	    if (matricola.length() != 9) {
+	    	System.out.println("Matricola non valido..");
+	        return "matricola";
+	    }
+	    
+	    if(isValidEmail(email) == 1) {
+	    	 System.out.println("Email non valido..");
+	    	 return "email";
+	    } 
+	    
+	    if (username.length() > 10) {
+	    	 System.out.println("Username non valido..");
+	        return "username";
+	    }
+	    if (password.length() < 8 || password.length() > 20) {
+	    	 System.out.println("Password non valido..");
+	        return "password";
+	    }
+	    // Se arrivi qui, tutti i campi sono validi
+	    return "Tutti i campi sono correttamente compilati.";
+	}
+	
+	private int isValidEmail(String email) {
+		
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches()) return 0;
+        
+        return 1;
+        
+
+	}
+	
+	private int isValidNameSurname(String s) {
+		
+		String sRegex = "^[A-Za-zÀ-ÿ'\\s]+$";
+
+        Pattern pattern = Pattern.compile(sRegex);
+        Matcher matcher = pattern.matcher(s);
+
+        if (matcher.matches()) return 0;
+        
+        return 1;
+        
+
+	}
 }
--- UninaSwap/src/application/RegistrazioneBoundary.java
+++ UninaSwap/src/application/RegistrazioneBoundary.java
@@ -9,7 +9,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -24,8 +26,15 @@ public class RegistrazioneBoundary {
 	@FXML private Button ButtonInfoReg;
 	@FXML private Button ButtonReturnRegistrazione;
 	@FXML private Pane PaneLabelInfoReg;
-
 	@FXML private Pane LinkFooterRegistrazione;
+	
+	//Campi di Reggistrazione
+	@FXML private TextField  nomeFieldReg;
+	@FXML private TextField  cognomeFieldReg;
+	@FXML private TextField  matricolaFieldReg;
+	@FXML private TextField  emailFieldReg;
+	@FXML private TextField  usernameFieldReg;
+	@FXML private PasswordField   passwordFieldReg;
 	
 	@FXML
 	public void MostraLogin (MouseEvent e) {
@@ -73,6 +82,54 @@ public class RegistrazioneBoundary {
 		ButtonReturnRegistrazione.setVisible(false);
 		ButtonInfoReg.setVisible(true);
 	}
+	
+	@FXML	
+	public void PrelevaDati(MouseEvent e) {
+		
+        String nome = nomeFieldReg.getText().trim();
+        String cognome = cognomeFieldReg.getText().trim();
+        String matricola = matricolaFieldReg.getText().trim();
+        String email = emailFieldReg.getText().trim();
+        String username = usernameFieldReg.getText().trim();
+        String password = passwordFieldReg.getText().trim();
+
+		if(nome == "" || cognome == "" ||  matricola == "" || email == "" || username ==  "" || password ==  "")
+			System.out.println("merda");
+		else {
+			String result = controller.checkData(nome,cognome,matricola,email,username,password);
+			
+	        switch (result) {
+            case "nome":
+                nomeFieldReg.clear();
+                nomeFieldReg.setPromptText("Nome non valido");
+                break;
+            case "cognome":
+                cognomeFieldReg.clear();
+                cognomeFieldReg.setPromptText("Cognome non valido");
+                break;
+            case "matricola":
+                matricolaFieldReg.clear();
+                matricolaFieldReg.setPromptText("Matricola non valida");
+                break;
+            case "email":
+                emailFieldReg.clear();
+                emailFieldReg.setPromptText("Email non valida");
+                break;
+            case "username":
+                usernameFieldReg.clear();
+                usernameFieldReg.setPromptText("Username non valido");
+                break;
+            case "password":
+                passwordFieldReg.clear();
+                passwordFieldReg.setPromptText("Password non valida");
+                break;
+            default: System.out.println("bene");
+	        }
+		}
+        // Qui puoi aggiungere logica di validazione o salvataggio
+		
+	}
+	
 	
 	@FXML
 	public void LinkLuigi(MouseEvent e) {
