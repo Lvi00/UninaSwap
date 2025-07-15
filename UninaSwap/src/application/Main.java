bf16b77fc26fbddd26c2a460a099a2bf69b72c05
casta <casta@Pc-Luigi.mshome.net>
2025-07-15 18:53:44 +0200

    test

--- UninaSwap/src/application/Main.java
+++ UninaSwap/src/application/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
+			
 			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
