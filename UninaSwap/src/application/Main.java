0ada92370aabaae0217540ecd6ce71cc447930c9
giuseeee88 <giuseppecaut57@gmail.com>
2025-07-26 18:16:01 +0200

    aaaa

--- UninaSwap/src/application/Main.java
+++ UninaSwap/src/application/Main.java
@@ -15,19 +15,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-        	//Ottengo le dimensioni dello schermo
-            Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-            double width = screenBounds.getWidth();
-            double height = screenBounds.getHeight();
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource("boundary/Login.fxml"));
             Parent root = loader.load();
-            LoginBoundary controller = loader.getController();
-
-            stage.setWidth(width);
-            stage.setHeight(height);
-            stage.setX(0);
-            stage.setY(0);
 
             Scene scene = new Scene(root, width, height);
             scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
@@ -36,10 +25,6 @@ public class Main extends Application {
             stage.setTitle("UninaSwap - Login");
             stage.getIcons().add(new Image(getClass().getResource("IMG/logoApp.png").toExternalForm()));
             stage.setResizable(false);
-            
-            // Imposta i nodi alla dimensione corretta
-            controller.setContainerSize(width, height);
-            controller.setAnchorPaneSize(width, height);
 
             stage.show();
         }
